a13568828e54670a81d36aeff3d29dc0b3e37bd0
Vikas <vikaskumarr365@gmail.com>
2025-08-23 00:38:09 +0530

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Chat from "./chat";
+import Chat from "./Chat";
 
 function App() {
   return (
