a6971b0d12b467656c33393b0746cfab39a14192
Vikas <vikaskumarr365@gmail.com>
2025-08-25 12:58:05 +0530

    added piyush persona

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Chat from "./Chat";
+import ChatPiyush from "./ChatPiyush";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/ChatPiyush" element={<ChatPiyush />} />
       </Routes>
     </Router>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,13 +6,27 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="home-container">
-      <img src="/profile.png" alt="My Profile" className="profile-pic" />
-      <h1>Or kya haal chal👋</h1>
-      <p>Haan ji bat karni hai toh click kariye</p>
-      <button className="chat-button" onClick={() => navigate("/chat")}>
-        💬 Chat with me
-      </button>
+    <div className="container">
+      <div className="home-container">
+        <img src="/profile.png" alt="My Profile" className="profile-pic" />
+        <h1>Or kya haal chal👋</h1>
+        <p>Haan ji chai par charcha kare</p>
+        <button className="chat-button" onClick={() => navigate("/chat")}>
+          Chat with me Hitesh
+        </button>
+      </div>
+      <div className="home-container">
+        <img
+          src="/4cfa97b3e1c9ce1a702c88e126edf90979f1cce0.jfif"
+          alt="My Profile"
+          className="profile-pic"
+        />
+        <h1>Kese ho aap log👋</h1>
+        <p>Kyn confuse ho bhai aap</p>
+        <button className="chat-button" onClick={() => navigate("/ChatPiyush")}>
+          Chat with me piyush
+        </button>
+      </div>
     </div>
   );
 }
